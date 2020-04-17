04ae6afe485821d00077749a209ad63c876c8ad9
iTsense <support@itsense.pk>
2020-04-17 14:06:43 +0500

    Lets Build Something Cool

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+// postcss.config.js
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./build/html/**/*.html",
+    // "./src/**/*.vue",
+    // "./src/**/*.jsx",
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    process.env.NODE_ENV === "production"
+      ? require("cssnano")({
+          preset: "default",
+        })
+      : null,
+  ],
+};
